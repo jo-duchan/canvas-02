5f8e09f23b5e935e74eec1c4bd0261fa6b9bcdab
jo-duchan <jo_duchan@icloud.com>
2022-12-13 18:35:40 +0900

    sheep-controller.js 작성

--- sheep-controller.js
+++ sheep-controller.js
@@ -1,14 +1,48 @@
+import { Sheep } from "./sheep.js";
+
 export class SheepController {
-  constructor() {}
+  constructor() {
+    this.img = new Image();
+    this.img.onload = () => {
+      this.loaded();
+    };
+    this.img.src = "sheep.png";
+    this.items = [];
+
+    this.cur = 0;
+    this.isLoaded = false;
+  }
 
   resize(stageWidth, stageHeight) {
     this.stageWidth = stageWidth;
     this.stageHeight = stageHeight;
   }
 
-  loaded() {}
+  loaded() {
+    this.isLoaded = true;
+    this.addSheep();
+  }
 
-  addSheep() {}
+  addSheep() {
+    this.items.push(new Sheep(this.img, this.stageWidth));
+  }
 
-  draw(ctx, t, dots) {}
+  draw(ctx, t, dots) {
+    if (this.isLoaded) {
+      this.cur += 1;
+      if (this.cur > 200) {
+        this.cur = 0;
+        this.addSheep();
+      }
+
+      for (let i = this.items.length - 1; i >= 0; i--) {
+        const item = this.itemsp[i];
+        if (this.x < -item.width) {
+          this.items.splice(i, 1);
+        } else {
+          item.draw(ctx, t, dots);
+        }
+      }
+    }
+  }
 }
