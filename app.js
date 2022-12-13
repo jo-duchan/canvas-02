c052f6b3ca21c3d63cb62f6813326337e9af25a2
jo-duchan <jo_duchan@icloud.com>
2022-12-13 15:46:22 +0900

    app.js 작성

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
 
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    requestAnimationFrame(this.animation.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+  }
+
+  animation(t) {
+    requestAnimationFrame(this.animation.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
