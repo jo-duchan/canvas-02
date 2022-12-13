008973d286938fb639899ea8e31da944402cd4a8
jo-duchan <jo_duchan@icloud.com>
2022-12-13 18:11:35 +0900

    Create Sheep-controller.js

---
+++ sheep-controller.js
@@ -0,0 +1,14 @@
+export class SheepController {
+  constructor() {}
+
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+  }
+
+  loaded() {}
+
+  addSheep() {}
+
+  draw(ctx, t, dots) {}
+}
